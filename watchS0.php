8da194504e96ad3d72ca91e6d151759e3db53533
Rainer Gauweiler <rgr@energie-und-it.de>
2014-04-23 13:28:21 +0200

    s0Watcher läuft

--- watchS0.php
+++ watchS0.php
@@ -7,18 +7,17 @@ $streams=array(
                 array("gpio"=> 23, "name"=> "S0/1", "resolution"=>1000),
                 array("gpio"=> 22, "name"=> "S0/2", "resolution"=>1000),
                 array("gpio"=> 24, "name"=> "S0/3", "resolution"=>1000),
-                array("gpio"=> 10, "name"=> "S0/4", "resolution"=>1000),
-                array("gpio"=> 09, "name"=> "S0/5", "resolution"=>1000)
+                array("gpio"=> 10, "name"=> "S0/4", "resolution"=>2000),
+                array("gpio"=> 9, "name"=> "S0/5", "resolution"=>1000)
               );
                                                                                                               
 //-------------------------------------------------
 // don't touch below
 
 class myEventSignal {
-    private $base, $ev,$filenames;
+    private $base, $ev;
     
-        public function __construct($base,&$filenames) {
-                $this->filenames = $filenames;
+        public function __construct($base) {
                 $this->base = $base;
                 $this->ev = Event::signal($base,SIGINT, array($this, 'eventSighandler'));
                 $this->ev->add();
@@ -32,22 +31,23 @@ class myEventSignal {
 }
 
 class myEventStream {
-    private $base, $ev,$stream,$streamName.$oldTimestamp,$resolution;
+    private $base, $ev,$stream,$streamName,$oldTimestamp,$resolution;
     
-        public function __construct($base,$filename,$stream,$streamName,$resolution) {
+        public function __construct($base,$filename,$streamName,$resolution) {
                 $this->base = $base;
                 $this->stream = fopen($filename,'r');
                 $this->streamName=$streamName;
                 $this->resolution=$resolution;
-                $this->powerFactorPerMilliSecond=$this->resolution/(1000*(60*60*1000))
-                $this->ev = new Event($base, $this->stream,Event::READ | Event::PERSIST | Event::ET, ,array($this,'eventStreamHandler'));
+                $this->powerFactorPerMilliSecond=((60*60*1000))/($this->resolution * 1000);
+                $this->ev = new Event($base, $this->stream , Event::READ | Event::PERSIST | Event::ET, array($this,'eventStreamHandler'));
                 $this->ev->add();
         } // __construct
                                     
         public function eventStreamHandler($args) {
                 $timestamp = getTimestamp();
                 if ($this->oldTimestamp<>0) {
-                   echo "Zeitdifferenz: " . $this->streamName.": ". ($timestamp - $this->oldTimestamp) ."ms\n" ;
+                   $timeDiff=($timestamp - $this->oldTimestamp);
+                   echo "Zeitdifferenz: " . $this->streamName.": $timeDiff ms entspricht ".$timeDiff*$this->powerFactorPerMilliSecond ." Watt\n" ;
                    echo "\n\n";
                 } // if
                 $this->oldTimestamp=$timestamp;                                                                                           
@@ -60,12 +60,13 @@ function getTimestamp() {
 }  // function getTimestamp
 
 $base = new EventBase();
-$c    = new myEventSignal($base,$filenames);
+$c    = new myEventSignal($base);
+$e = array();
 
-foreach($filenames as $i) {
- $e[$i] = new myEventTimer($base,$timeToWait,$i);
- usleep(330);
- $base->loop(EventBase::LOOP_ONCE);
+foreach($streams as $i) {
+ $filename="/sys/class/gpio/gpio".$i["gpio"]."/value";
+ echo "Filename: $filename\n";
+ $e[] = new myEventStream($base,$filename,$i["name"],$i["resolution"]);
 } // foreach
 $base->loop();
 
